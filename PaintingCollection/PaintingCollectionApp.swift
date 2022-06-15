620b9f56a68d17dbc501a0957393a12b7675b9a4
anyaachan <anna.danchenko@icloud.com>
2022-06-15 18:41:22 +0300

    Initial Commit

---
+++ PaintingCollection/PaintingCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintingCollectionApp.swift
+//  PaintingCollection
+//
+//  Created by Anna-Alexandra Danchenko on 15.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PaintingCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
